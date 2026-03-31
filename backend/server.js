fe9aff3dddeb419bd26c88589e6736bfbe9294a2
Younis <ya945055@gmail.com>
2026-03-31 22:26:11 +0500

    Refactor server.js to define PORT constant for improved readability

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const connectDb = require('./src/db/db');
 
 connectDb();
 
-app.listen(process.env.PORT || 3000 , () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 });
 
